787d953ab4b7275417ad808428588b369a53fe50
Yesung Lee <yesung5337@naver.com>
2024-05-08 00:11:20 +0900

    초기 프로젝트파일 정보

---
+++ Assets/LoginScript/LoginSceneManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TJ
+{
+public class LoginSceneManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject signScene;
+    public GameObject loginScene;
+    public GameObject Main;
+
+
+
+    public void ignupButton()
+    {
+        loginScene.SetActive(false);
+    }
+
+    public void SignupButton_()
+    {
+        loginScene.SetActive(true);
+    }
+
+
+}
+}
